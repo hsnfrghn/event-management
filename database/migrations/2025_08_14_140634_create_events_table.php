b54485d7b9042e1d858297a71178d601d6519304
Hasan Forghani <hasan.forghani25@gmail.com>
2025-08-19 13:33:27 +0330

    db factories and seeders for events and attendees

--- database/migrations/2025_08_14_140634_create_events_table.php
+++ database/migrations/2025_08_14_140634_create_events_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId(User::class);
+            $table->foreignIdFor(User::class);
             $table->string('name');
             $table->text('description')->nullable();
             $table->dateTime('start_time');
--- database/migrations/2025_08_14_140714_create_attendees_table.php
+++ database/migrations/2025_08_14_140714_create_attendees_table.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,8 +15,8 @@ return new class extends Migration
         Schema::create('attendees', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId(User::class);
-            $table->foreignId(Event::class);
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Event::class);
             $table->timestamps();
         });
     }
