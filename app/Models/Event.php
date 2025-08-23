aaed20593bd62b92c0d8d8c1d7e2af6c61a522a7
Hasan Forghani <hasan.forghani25@gmail.com>
2025-08-23 16:22:34 +0330

    adding fillable to Event class for mass asignment

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -11,6 +11,8 @@ class Event extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'description', 'start_date', 'end_date', 'user_id'];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
