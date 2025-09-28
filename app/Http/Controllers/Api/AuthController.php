c0ec23e6393cac9b4e675e7f41af856ccdbc6a42
Hasan Forghani <hasan.forghani25@gmail.com>
2025-09-28 13:52:40 +0330

    revoking tokens and logout

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -37,6 +37,8 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
+        $request->user()->tokens()->delete();
 
+        return response()->json(['message' => 'You have been logged out!']);
     }
 }
